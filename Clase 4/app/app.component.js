a36a48242e745be5af8b370a0b1452f34c61bec5
yacaFx <sergiobritor@gmail.com>
2016-08-05 18:33:22 -0500

    Files for class 4

--- Clase 4/app/app.component.js
+++ Clase 4/app/app.component.js
@@ -14,23 +14,39 @@ var Pokemon = (function () {
     }
     return Pokemon;
 }());
-exports.Pokemon = Pokemon;
 var AppComponent = (function () {
     function AppComponent() {
         this.titleApp = "Pokedex";
         this.message = "Sin mensaje";
-        this.pickachu = {
-            id: 1,
-            name: 'Pikachu',
-            mainPower: 'Attack Trueno',
-            type: "Electrico",
-            status: true
+        this.pokemons = [];
+        this.selectedPokemon = {
+            id: 0,
+            name: '-',
+            mainPower: '-',
+            type: '-',
+            status: false
         };
+        this.pokemon = new Pokemon();
     }
+    AppComponent.prototype.addPokemon = function () {
+        var locPokemon = new Pokemon();
+        locPokemon.id = this.pokemons.length;
+        locPokemon.name = this.pokemon.name;
+        locPokemon.mainPower = this.pokemon.mainPower;
+        locPokemon.type = this.pokemon.type;
+        locPokemon.status = this.pokemon.status;
+        this.pokemons.push(locPokemon);
+        console.log(this.pokemons);
+    };
+    AppComponent.prototype.showPokemonDetail = function (pokemon) {
+        console.log(pokemon);
+        this.selectedPokemon = pokemon;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <h1>My {{ titleApp }}</h1>\n  {{ 2 + 2 }}\n  <div>\n    {{ pickachu.name }}\n    {{ pickachu.mainPower }}\n    {{ pickachu.type }}\n    {{ pickachu.status }}\n  </div>\n  "
+            template: "\n  <h1>My {{ titleApp }}</h1>\n\n<input [(ngModel)]=\"pokemon.name\" type=\"text\" placeholder=\"Nombre de pokemon\" >\n<input [(ngModel)]=\"pokemon.mainPower\" type=\"text\" placeholder=\"Poder\" >\n<input [(ngModel)]=\"pokemon.type\" type=\"text\" placeholder=\"Tipo\" >\n<input [(ngModel)]=\"pokemon.status\" type=\"text\" placeholder=\"Estatus\" >\n  <button (click)=\"addPokemon()\">Nuevo Pokemon</button>\n  <ul>\n    <li (click)=\"showPokemonDetail(pokemon)\"\n        *ngFor=\"let pokemon of pokemons;  let i = index\"\n        [class.selected]=\"pokemon === selectedPokemon\"\n        >\n    {{ i }}  {{ pokemon.name }}\n    </li>\n  </ul>\n  <div class=\"pokemon\">\n    <span>{{selectedPokemon.id}}</span>\n    <span>{{selectedPokemon.name}}</span>\n    <span>{{selectedPokemon.mainPower}}</span>\n    <span>{{selectedPokemon.type}}</span>\n    <span>{{selectedPokemon.status}}</span>\n  </div>\n  ",
+            styles: ["\n      .pokemon{\n        background-color: yellow;\n        color: black;\n        width: 300px;\n        height: 100px;\n        padding: 20px;\n        text-align: center;\n      }\n      span{\n        display: block;\n      }\n      .selected{\n        background-color: blue;\n        color: white;\n        font-weight: bold;\n      }\n\n    "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- Clase 4/app/app.component.ts
+++ Clase 4/app/app.component.ts
@@ -1,39 +1,90 @@
 import { Component } from '@angular/core';
 
-export class Pokemon{
-  id: number;
-  name: string;
-  mainPower: string;
-  type: string;
-  status: boolean;
+class Pokemon {
+    id: number;
+    name: string;
+    mainPower: string;
+    type: string;
+    status: boolean;
 }
 
 @Component({
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
   <h1>My {{ titleApp }}</h1>
-  {{ 2 + 2 }}
-  <div>
-    {{ pickachu.name }}
-    {{ pickachu.mainPower }}
-    {{ pickachu.type }}
-    {{ pickachu.status }}
+
+<input [(ngModel)]="pokemon.name" type="text" placeholder="Nombre de pokemon" >
+<input [(ngModel)]="pokemon.mainPower" type="text" placeholder="Poder" >
+<input [(ngModel)]="pokemon.type" type="text" placeholder="Tipo" >
+<input [(ngModel)]="pokemon.status" type="text" placeholder="Estatus" >
+  <button (click)="addPokemon()">Nuevo Pokemon</button>
+  <ul>
+    <li (click)="showPokemonDetail(pokemon)"
+        *ngFor="let pokemon of pokemons;  let i = index"
+        [class.selected]="pokemon === selectedPokemon"
+        >
+    {{ i }}  {{ pokemon.name }}
+    </li>
+  </ul>
+  <div class="pokemon">
+    <span>{{selectedPokemon.id}}</span>
+    <span>{{selectedPokemon.name}}</span>
+    <span>{{selectedPokemon.mainPower}}</span>
+    <span>{{selectedPokemon.type}}</span>
+    <span>{{selectedPokemon.status}}</span>
   </div>
-  `
+  `,
+    styles: [`
+      .pokemon{
+        background-color: yellow;
+        color: black;
+        width: 300px;
+        height: 100px;
+        padding: 20px;
+        text-align: center;
+      }
+      span{
+        display: block;
+      }
+      .selected{
+        background-color: blue;
+        color: white;
+        font-weight: bold;
+      }
+
+    `]
 })
 
 
 export class AppComponent {
-  titleApp = "Pokedex";
-  message = "Sin mensaje";
+    titleApp = "Pokedex";
+    message = "Sin mensaje";
+    pokemons: Pokemon[] = [];
+    selectedPokemon: Pokemon = {
+      id:0,
+      name:'-',
+      mainPower:'-',
+      type:'-',
+      status:false
+    };
 
-  pickachu: Pokemon = {
-    id: 1,
-    name: 'Pikachu',
-    mainPower: 'Attack Trueno',
-    type: "Electrico",
-    status: true
-  };
+    pokemon = new Pokemon()
 
+    addPokemon() {
+        let locPokemon = new Pokemon();
+        locPokemon.id = this.pokemons.length;
+        locPokemon.name = this.pokemon.name;
+
+        locPokemon.mainPower = this.pokemon.mainPower;
+        locPokemon.type = this.pokemon.type;
+        locPokemon.status = this.pokemon.status;
+
+        this.pokemons.push(locPokemon);
+        console.log(this.pokemons)
+    }
+    showPokemonDetail(pokemon) {
+        console.log(pokemon)
+        this.selectedPokemon = pokemon
+    }
 
 }
